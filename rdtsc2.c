b8dff32cd5eb69a0490870b4ec8aa17bed879f95
Yuanchao Zhu <zyc262626@gmail.com>
2016-02-01 15:25:15 -0800

    Cycle time

--- rdtsc2.c
+++ rdtsc2.c
@@ -81,5 +81,20 @@ int main(int argc, char* argv[])
 
     printf("The average overhead is: %f cycles\n", sum / loops);
 
+    asm volatile ("CPUID\n\t"
+            "RDTSC\n\t"
+            "mov %%edx, %0\n\t"
+            "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+            "%rax", "%rbx", "%rcx", "%rdx");
+    sleep(10);
+    asm volatile("RDTSCP\n\t"
+            "mov %%edx, %0\n\t"
+            "mov %%eax, %1\n\t"
+            "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
+            "%rbx", "%rcx", "%rdx");
+    start = (((uint64_t)cycles_high << 32)| cycles_low );
+    end= (((uint64_t)cycles_high1<< 32) | cycles_low1 );
+    printf("The 10s sleep is %" PRIu64 " cycles\n", (end - start));
+
     return 0;
 }
