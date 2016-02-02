8fef53ac8d560d00875c1cad32230d8e513d2729
Weichen Liu <kevbtc@gmail.com>
2016-02-01 16:40:57 -0800

    syscall

--- syscall.c
+++ syscall.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <inttypes.h>
 
 static inline unsigned long long tick() 
 {
@@ -12,6 +13,7 @@ static inline unsigned long long tick()
 int main(int argc, char* argv[])
 {
     int loops;
+    int i, j;
     if (argc <= 1) {
         exit(0);
     }
@@ -23,13 +25,107 @@ int main(int argc, char* argv[])
         }
     }
 
-    unsigned long long start, end;
-    int i;
-    start = tick();
-    for(i=0; i < loops; i++) {
+    uint64_t start, end;
+    unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
+    uint64_t *times;
+
+    times = malloc(loops * sizeof(uint64_t*));
+
+    // Warm up the system
+    asm volatile ("CPUID\n\t"
+            "RDTSC\n\t"
+            "mov %%edx, %0\n\t"
+            "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+            "%rax", "%rbx", "%rcx", "%rdx");
+    asm volatile("RDTSCP\n\t"
+            "mov %%edx, %0\n\t"
+            "mov %%eax, %1\n\t"
+            "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
+            "%rbx", "%rcx", "%rdx");
+
+    asm volatile ("CPUID\n\t"
+            "RDTSC\n\t"
+            "mov %%edx, %0\n\t"
+            "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+            "%rax", "%rbx", "%rcx", "%rdx");
+    asm volatile("RDTSCP\n\t"
+            "mov %%edx, %0\n\t"
+            "mov %%eax, %1\n\t"
+            "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
+            "%rbx", "%rcx", "%rdx");
+
+
+    // test without loop
+            asm volatile ("CPUID\n\t"
+                "RDTSC\n\t"
+                "mov %%edx, %0\n\t"
+                "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+                "%rax", "%rbx", "%rcx", "%rdx");
+
+    getppid();
+
+            asm volatile("RDTSCP\n\t"
+                "mov %%edx, %0\n\t"
+                "mov %%eax, %1\n\t"
+                "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
+                "%rbx", "%rcx", "%rdx");
+
+        start = (((uint64_t)cycles_high << 32)| cycles_low );
+        end= (((uint64_t)cycles_high1<< 32) | cycles_low1 );
+        times[0] =end - start;
+
+    // test without loop x2
+
+                asm volatile ("CPUID\n\t"
+                "RDTSC\n\t"
+                "mov %%edx, %0\n\t"
+                "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+                "%rax", "%rbx", "%rcx", "%rdx");
+
+    getppid();
+
+            asm volatile("RDTSCP\n\t"
+                "mov %%edx, %0\n\t"
+                "mov %%eax, %1\n\t"
+                "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
+                "%rbx", "%rcx", "%rdx");
+
+        start = (((uint64_t)cycles_high << 32)| cycles_low );
+        end= (((uint64_t)cycles_high1<< 32) | cycles_low1 );
+        times[1] =end - start;
+
+    // loop
+
+    for(i = 2; i < loops+2; i++) {
+        asm volatile ("CPUID\n\t"
+                "RDTSC\n\t"
+                "mov %%edx, %0\n\t"
+                "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+                "%rax", "%rbx", "%rcx", "%rdx");
+
+        // Call the function to measure here
         getppid();
+
+        asm volatile("RDTSCP\n\t"
+                "mov %%edx, %0\n\t"
+                "mov %%eax, %1\n\t"
+                "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
+                "%rbx", "%rcx", "%rdx");
+
+        start = (((uint64_t)cycles_high << 32)| cycles_low );
+        end= (((uint64_t)cycles_high1<< 32) | cycles_low1 );
+        times[i] =end - start;
     }
-    end = tick();
-    printf("argv: %d, overhead: %f\n", loops, (double)(end- start) / (double)loops);
+
+    double sum = 0;
+
+    // filter first time
+    for (i = 1; i < loops+2; i++) {
+        sum += times[i];
+        printf("Loop %d: overhead = %" PRIu64 " cycles\n", i, times[i]);
+    }
+
+    printf("The average overhead is: %f cycles\n", sum / loops);
+
     return 0;
 }
