97b699e338c7dbbccccd78df5532e9383075cec8
Yuanchao Zhu <zyc262626@gmail.com>
2016-02-01 19:38:55 -0800

    pthread

---
+++ pthread_context_switch.c
@@ -0,0 +1,104 @@
+#include<pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <unistd.h>
+
+pthread_t tid;
+
+// Pipe
+int fd[2];
+
+void write_fd(void *arg) {
+    write(fd[1], "a", 1);
+    pthread_exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    int loops;
+    int i, j;
+
+    uint64_t *times;
+    if (argc <= 1) {
+        exit(0);
+    }
+    else {
+        loops = atoi (argv[1]);
+        if (loops <= 0) {
+            printf ("Invalid arguments.\n");
+            exit(0);
+        }
+    }
+
+    times = malloc(loops*sizeof(uint64_t*));
+
+    uint64_t start, end;
+    unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
+
+    char buffer;
+
+    if (pipe(fd) == -1) {
+        perror("Pipe setup error!\n");
+        exit(1);
+    }
+
+        // Warm up the system
+    asm volatile ("CPUID\n\t"
+            "RDTSC\n\t"
+            "mov %%edx, %0\n\t"
+            "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+            "%rax", "%rbx", "%rcx", "%rdx");
+    asm volatile("RDTSCP\n\t"
+            "mov %%edx, %0\n\t"
+            "mov %%eax, %1\n\t"
+            "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
+            "%rbx", "%rcx", "%rdx");
+
+    asm volatile ("CPUID\n\t"
+            "RDTSC\n\t"
+            "mov %%edx, %0\n\t"
+            "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+            "%rax", "%rbx", "%rcx", "%rdx");
+    asm volatile("RDTSCP\n\t"
+            "mov %%edx, %0\n\t"
+            "mov %%eax, %1\n\t"
+            "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
+            "%rbx", "%rcx", "%rdx");
+
+    for (i = 0; i < loops; i++) {
+        int err = pthread_create(&tid, NULL, (void *) &write_fd, NULL);
+        if (err != 0) {
+            perror("Pthread creation failed!\n");
+            exit(1);
+        }
+
+        asm volatile ("CPUID\n\t"
+                "RDTSC\n\t"
+                "mov %%edx, %0\n\t"
+                "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+                "%rax", "%rbx", "%rcx", "%rdx");
+
+        read(fd[0], &buffer, 1);
+
+        asm volatile("RDTSCP\n\t"
+                "mov %%edx, %0\n\t"
+                "mov %%eax, %1\n\t"
+                "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
+                "%rbx", "%rcx", "%rdx");
+
+        start = (((uint64_t)cycles_high << 32)| cycles_low );
+        end= (((uint64_t)cycles_high1<< 32) | cycles_low1 );
+        times[i] = end - start;
+    }
+
+    double sum = 0.0;
+    for (i = 0; i < loops; i++) {
+        sum += times[i];
+        printf("Loop %d: overhead = %" PRIu64 " cycles\n", i, times[i]);
+    }
+    printf("The average overhead is: %f cycles\n", sum / loops); 
+
+    return 0;
+}
