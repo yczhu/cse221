6e5eb4f8fb2af8d0d21741317ee4330a0cad846c
Yuanchao Zhu <zyc262626@gmail.com>
2016-02-20 18:46:11 -0800

    pthread context switch with wait and signal

--- pthread_context_switch.c
+++ pthread_context_switch.c
@@ -6,12 +6,18 @@
 #include <unistd.h>
 
 pthread_t tid;
+int test_cond = 0;
 
-// Pipe
-int fd[2];
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 void write_fd(void *arg) {
-    write(fd[1], "a", 1);
+    pthread_mutex_lock(&mut);
+    //printf("Pthread wait!\n");
+    //test_cond = 1;
+    pthread_cond_wait(&cond, &mut);
+    //printf("Pthread wait done!\n");
+    pthread_mutex_unlock(&mut);
     pthread_exit(0);
 }
 
@@ -37,13 +43,6 @@ int main(int argc, char *argv[])
     uint64_t start, end;
     unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
 
-    char buffer;
-
-    if (pipe(fd) == -1) {
-        perror("Pipe setup error!\n");
-        exit(1);
-    }
-
         // Warm up the system
     asm volatile ("CPUID\n\t"
             "RDTSC\n\t"
@@ -67,26 +66,32 @@ int main(int argc, char *argv[])
             "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
             "%rbx", "%rcx", "%rdx");
 
+
     for (i = 0; i < loops; i++) {
+        test_cond = 0;
         int err = pthread_create(&tid, NULL, (void *) &write_fd, NULL);
+
         if (err != 0) {
             perror("Pthread creation failed!\n");
             exit(1);
         }
 
-        asm volatile ("CPUID\n\t"
-                "RDTSC\n\t"
-                "mov %%edx, %0\n\t"
-                "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
-                "%rax", "%rbx", "%rcx", "%rdx");
+        //printf("Main ready to signal\n");
+        pthread_mutex_lock(&mut);
+            asm volatile ("CPUID\n\t"
+                    "RDTSC\n\t"
+                    "mov %%edx, %0\n\t"
+                    "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+                    "%rax", "%rbx", "%rcx", "%rdx");
+            pthread_cond_signal(&cond);
+            asm volatile("RDTSCP\n\t"
+                    "mov %%edx, %0\n\t"
+                    "mov %%eax, %1\n\t"
+                    "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
+                    "%rbx", "%rcx", "%rdx");
+        pthread_mutex_unlock(&mut);
 
-        read(fd[0], &buffer, 1);
-
-        asm volatile("RDTSCP\n\t"
-                "mov %%edx, %0\n\t"
-                "mov %%eax, %1\n\t"
-                "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
-                "%rbx", "%rcx", "%rdx");
+        //printf("Back to main!\n");
 
         start = (((uint64_t)cycles_high << 32)| cycles_low );
         end= (((uint64_t)cycles_high1<< 32) | cycles_low1 );
