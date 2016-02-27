088a8a457644f051223b0b4806142f4184816cc5
Yuanchao Zhu <zyc262626@gmail.com>
2016-02-27 14:15:37 -0800

    RTT

---
+++ network/rtt-client.py
@@ -0,0 +1,29 @@
+"""
+A RTT test on TCP server
+"""
+import socket
+import time
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+#MESSAGE = "Hello, World!"
+MESSAGE = "X" * 200
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.connect((TCP_IP, TCP_PORT))
+
+i = 0
+numerofpackets = 10
+
+while i < numerofpackets:
+    start_time = time.time()
+    s.send(MESSAGE)
+    response = s.recv(BUFFER_SIZE)
+    #print response
+    print (time.time() - start_time) * 1000
+    i = i + 1
+
+print "Close"
+s.close()
+
