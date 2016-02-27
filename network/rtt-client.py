bcb5c0866e9a4d4ec75f129dccf223b2baa233d1
Yuanchao Zhu <zyc262626@gmail.com>
2016-02-27 14:46:49 -0800

    Server bind ip 0.0.0.0

--- network/rtt-client.py
+++ network/rtt-client.py
@@ -4,11 +4,12 @@ A RTT test on TCP server
 import socket
 import time
 
-TCP_IP = '127.0.0.1'
+#TCP_IP = '127.0.0.1'
+TCP_IP = '45.55.2.40'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 #MESSAGE = "Hello, World!"
-MESSAGE = "X" * 200
+MESSAGE = "X" * 56
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 s.connect((TCP_IP, TCP_PORT))
